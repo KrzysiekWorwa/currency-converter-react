682213c803e2b7e883038ed9deaa46d7245a004b
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-04 13:11:10 +0100

    adding currencies array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import Form from "./Form";
 
 
 function App() {
+
+const [result, setResult] = useState();
+
+const calculateResult = (currency, amount) => {
+  const rate = currencies
+  .find(({short}) => short === currency)
+  .rate;
+
+  setResult({
+    sourceAmount: +amount,
+    targetAmount: amount / rate,
+    currency,
+  });
+}
+
   return (
     <Container>
      <Form/>
