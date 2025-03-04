19a1527e9dc1356bc1bd8a4e6d0abca5e1db04e6
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-04 14:58:05 +0100

    removing container for better visibility

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import Container from "./Container";
 import Form from "./Form";
 import { currencies } from "./currencies";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 function App() {
 
-const [result, setResult] = useState();
+  const [result, setResult] = useState();
 
-const calculateResult = (currency, amount) => {
-  const rate = currencies
-  .find(({short}) => short === currency)
-  .rate;
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
 
-  setResult({
-    sourceAmount: +amount,
-    targetAmount: amount / rate,
-    currency,
-  });
-}
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
 
   return (
-    <Container>
-     <Form
-     result={result}
-     calculateResult={calculateResult}
-     />
-      </Container>
+    <div className="container">
+      <Form
+        result={result}
+        calculateResult={calculateResult}
+      />
+    </div>
   );
 }
 
