882cf500649d3cacaa3c47840060d710b32185e3
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-06 20:03:51 +0100

    adding date counter

--- src/Form/Date/index.js
+++ src/Form/Date/index.js
@@ -1,19 +1,32 @@
+import React, { useState, useEffect } from "react";
 import "./style.css"
-export const CurrentDate = () => {
-    const date = new Date();
-  
-const day = date.toLocaleDateString("pl", {
-  weekday: "long", 
-  day: "numeric", 
-  month: "long", 
-  year: "numeric"
-});
 
-const time = date.toLocaleTimeString();
 
-const dateAndTime = `${day}, ${time}`;
+export const DateCounter = () => {
 
-return(
-    <div className="date">Dzisiaj jest {dateAndTime}</div>
-);
+    const [currentDate, setCurrentDate] = useState(new Date());
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const day = currentDate.toLocaleDateString("pl", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+    const time = currentDate.toLocaleTimeString();
+    const dateAndTime = `${day}, ${time}`;
+
+
+    return (
+        <div className="date">Dzisiaj jest {dateAndTime}</div>
+    );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import React, { useState } from "react";
 import { currencies } from "../currencies";
-import {Result} from "./Result";
-import { CurrentDate } from "./Date";
+import { Result } from "./Result";
+import { DateCounter } from "./Date";
 
 export const Form = ({ calculateResult, result }) => {
   const [currency, setCurrency] = useState(currencies[0].short);
@@ -13,52 +13,52 @@ export const Form = ({ calculateResult, result }) => {
     calculateResult(currency, amount);
 
   }
-    return (
-        <form className="form" onSubmit={onSubmit}>
-        <CurrentDate />
-        <h1 className="form__header">Kalkulator walut</h1>
-        <p>
-          <label>
-            <span className="form__labelText">
-              Kwota w zł*
-            </span>
-            <input 
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <DateCounter />
+      <h1 className="form__header">Kalkulator walut</h1>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Kwota w zł*
+          </span>
+          <input
             value={amount}
-            onChange={({target}) => setAmount(target.value)}
-            className="form__field" 
-            placeholder="Wpisz kwotę w złotówkach" 
-            type="number" 
+            onChange={({ target }) => setAmount(target.value)}
+            className="form__field"
+            placeholder="Wpisz kwotę w złotówkach"
+            type="number"
             required
             step="0.01" />
-          </label>
-        </p>
-        <p>
-          <label>
-            <span className="form__labelText">
-              Waluta
-            </span>
-            <select 
-            className="form__field" 
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="form__labelText">
+            Waluta
+          </span>
+          <select
+            className="form__field"
             value={currency}
-            onChange={({target}) => setCurrency(target.value)}
-            >
-              {currencies.map((currency => (
+            onChange={({ target }) => setCurrency(target.value)}
+          >
+            {currencies.map((currency => (
               <option key={currency.short}
-              value={currency.short}
+                value={currency.short}
               >
                 {currency.name}
               </option>
             )))}
-            </select>
-          </label>
-        </p>
-        <p>
-          <button className="form__button">Przelicz!</button>
-        </p>
-        <p className="form__info">Kursy pochodzą ze strony nbp.pl</p>
-        <Result result={result}/>
-      </form>
-    );
+          </select>
+        </label>
+      </p>
+      <p>
+        <button className="form__button">Przelicz!</button>
+      </p>
+      <p className="form__info">Kursy pochodzą ze strony nbp.pl</p>
+      <Result result={result} />
+    </form>
+  );
 };
 
 export default Form;
