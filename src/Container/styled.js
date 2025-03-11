0aeec1291366a905444c0653b2cad82890dd76ae
Krzysztof Worwa <worwak1998@gmail.com>
2025-03-11 20:25:59 +0100

    adding Container styled.js

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,8 +1,10 @@
-.container {
+import styled from "styled-components";
+
+export const ContainerBody = styled.main`
     flex-basis: 700px;
     padding: 10px;
     margin: 5px;
     text-align: center;
     background-color: rgba(0, 0, 0, 0.5);
     border-radius: 20px;
-  }
+`;
